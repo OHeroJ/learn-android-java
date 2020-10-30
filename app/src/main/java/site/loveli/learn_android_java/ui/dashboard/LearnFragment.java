f9d6870edf96fcaf366ea8b6fce01af3fc487086
laijihua <1164258202@qq.com>
2020-10-30 09:22:03 +0800

    Feat: 重新布局项目

---
+++ app/src/main/java/site/loveli/learn_android_java/ui/dashboard/LearnFragment.java
@@ -0,0 +1,35 @@
+package site.loveli.learn_android_java.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import site.loveli.learn_android_java.R;
+
+public class LearnFragment extends Fragment {
+
+    private LearnViewModel learnViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        learnViewModel =
+                new ViewModelProvider(this).get(LearnViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_learn, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        learnViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
---
+++ app/src/main/java/site/loveli/learn_android_java/ui/dashboard/LearnViewModel.java
@@ -0,0 +1,19 @@
+package site.loveli.learn_android_java.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class LearnViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public LearnViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
